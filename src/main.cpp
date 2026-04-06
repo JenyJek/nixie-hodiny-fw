93bd73c0951678740f23493c427036204272376d
JenyJek <jena4mail@gmail.com>
2026-04-06 14:07:29 +0200

    little cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ void testAllSegments();
 void setup() {
   // setup se spousti pouze jednou, pri spusteni MCU (reset, napajeni apod.)
   Serial.begin(115200); //inicializuj seriovou linku s rychlosti 115200baud
-  //initMelodies(); //inicializace melodii
+  initMelodies(); //inicializace melodii
 
   //nastaveni modu GPIO
   pinMode(PIN_MH141_A1, OUTPUT);
@@ -52,24 +52,20 @@ void setup() {
   TIMSK1 |= (1 << OCIE1A);  // zapnout funkci timer compare
   interrupts(); //zapnout zpet vsechny interrupty
 
-  /*touch.Setup(PIN_TOUCH, 100, 40, 8); //nastavenicko captouch s defaultnima hodnotama
+  touch.Setup(PIN_TOUCH, 100, 40, 8); //nastavenicko captouch s defaultnima hodnotama
 
-  displayBuffer.digits[0] = 0;
-  displayBuffer.digits[1] = 0;
-  displayBuffer.digits[2] = 0;
-  displayBuffer.digits[3] = 0;
-  displayBuffer.digits[4] = 0;
-  displayBuffer.digits[5] = 0;
-  
+  for(uint8_t digit : displayBuffer.digits){
+    displayBuffer.digits[digit] = 0;
+  }
+    
   display.TurnOff();
-  //display.OnUpdate();
-  //Wire.begin();
-  //rtc.initAlm1();
+  display.OnUpdate();
+  Wire.begin();
+  rtc.initAlm1();
   Serial.println("startup done");
   displaying = MODE_TIME;
   display.TurnOn();
-  toneMachine.currentMelody = melodies.okSfxMelody;*/
-  digitalWrite(PIN_HV_MOS, HIGH);
+  toneMachine.currentMelody = melodies.okSfxMelody;
   testAllSegments();
 }
 
@@ -79,8 +75,9 @@ uint8_t anode, cathode = 0;
 uint8_t mode = 0;
 bool step;
 void loop(){
-  //toneMachine.loop();
-  //display.OnUpdate(); //zavolat update displeje
+  toneMachine.loop();
+  rtc.read();
+  display.OnUpdate(); //zavolat update displeje
   /*(millis() - lastMillis >= 100){
     //kazdych 100ms
     Serial.print("actual: ");
@@ -100,8 +97,8 @@ void loop(){
     Serial.println(displayBuffer.digits[5] == 0xF ?  '-' : char(displayBuffer.digits[5] + 48));
     lastMillis = millis();
   }*/
- /* if(millis() - lastMillis2 >= 50){
-    if(rtc.getAlm1FlagTrigger()){
+ if(millis() - lastMillis2 >= 500){
+    if(/*rtc.getAlm1FlagTrigger()*/true){
       // on alm1 trg flag
       if(displaying == MODE_TIME){
         display.SetDots(display.TIME);
@@ -136,18 +133,6 @@ void loop(){
   }
 
   if(millis() - lastMillis3 >= 25){
-    //kazdych 25ms
-    touch.Read();
-
-    if(lastTouch != touch.touched){
-      toneMachine.run = touch.touched;
-      lastTouch = touch.touched;
-    }
-    
-    lastMillis3 = millis();
-  }
-  */
-  /*if(millis() - lastMillis3 >= 25){
     //Serial.println("touch!");
     //kazdych 25ms
     touch.Read();
@@ -160,13 +145,12 @@ void loop(){
     }
     
     lastMillis3 = millis();
-  }*/
+  }
 }
 
 void testAllSegments() {
-  // 1. Create an array of pointers to your volatile variables
-  // This maps the loop index to your specific global variables
-  volatile uint_fast8_t* tubes[] = {
+  digitalWrite(PIN_HV_MOS, HIGH);
+  volatile uint8_t* tubes[] = {
     &displayBuffer.displayedHoursTens,   // Index 0
     &displayBuffer.displayedHoursUnits,  // Index 1
     &displayBuffer.displayedMinutesTens, // Index 2
@@ -174,57 +158,22 @@ void testAllSegments() {
     &displayBuffer.displayedSecondsTens, // Index 4
     &displayBuffer.displayedSecondsUnits // Index 5
   };
-
-  // 2. Set all digits to 0x0F (Blank) initially
   for (int i = 0; i < 6; i++) {
     *tubes[i] = 0x0F;
   }
-  delay(1000); // Short pause to see the blank state
-  /*for (int d = 0; d <= 15; d++) {
-      *tubes[0] = d;
-      
-      delay(200); // Speed of the count-up (adjust as needed)
-    }*/
-  /*//tohle to je tady jenom protoze jsem hledal hodnoty co kam do LUT
-  *tubes[5] = 0x08; //0
-  delay(1000);
-  *tubes[5] = 0x04; //1
-  delay(1000);
-  *tubes[5] = 0x0C; //2
-  delay(1000);
-  *tubes[5] = 0x02; //3
-  delay(1000);
-  *tubes[5] = 0x0A; //4
-  delay(1000);
-  *tubes[5] = 0x06; //5
-  delay(1000);
-  *tubes[5] = 0x0E; //6
-  delay(1000);
-  *tubes[5] = 0x01; //7
-  delay(1000);
-  *tubes[5] = 0x09; //8
-  delay(1000);
-  *tubes[5] = 0x00; //9
-  delay(1000);
-  *tubes[5] = 0x0F; //blank
-*/
-  // 3. Nested loop: For every tube...
+  delay(250);
   for (int t = 0; t < 6; t++) {
-    // ...for every digit 0-9
     for (int d = 0; d <= 9; d++) {
       *tubes[t] = d;
-      
-      delay(200); // Speed of the count-up (adjust as needed)
+      delay(50); 
     }
-    
-    // Optional: Blank the tube again before moving to the next 
-    // so you only see one tube active at a time
-    *tubes[t] = 0x0F; 
+     *tubes[t] = 0x0F; 
   }
+  digitalWrite(PIN_HV_MOS, LOW);
 }
 
-#define ONTIME 3
-#define OFFTIME 1
+#define ONTIME 4
+#define OFFTIME 2
 //tady se pouzije volatile aby sepredeslo memory optimization
 volatile uint_fast8_t pairIndex, timer_counter;
 volatile bool isBlanking;
@@ -232,7 +181,6 @@ volatile const uint_fast8_t nix_LUT_upper[]={0x00, 0x04, 0x0C, 0x02, 0x0A, 0x06,
 volatile const uint_fast8_t nix_LUT_downer[]={0x08, 0x04, 0x0C, 0x02, 0x0A, 0x06, 0x0E, 0x01, 0x09, 0x00, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F}; //a druha 74141 taky protoze jenyjek neni schopnej je zapojit stejne womp womp
 
 ISR(TIMER1_COMPA_vect) {
-  // there should be a newer version of this code @ my gemini
   if (--timer_counter == 0) {
     if (!isBlanking) {
       // --- DEAD-TIME  cas, kdy jsou vsechny anody vypnuty---
@@ -242,7 +190,7 @@ ISR(TIMER1_COMPA_vect) {
 
       // 2. na obe MH74141 nastav (0x0F - 0b1111) tj. vypnuto
       PORTD = (PORTD & 0x0F) | (0x0F << 4); // 1 (PD4-7)
-      PORTB = (PORTB & 0xF0) | 0x0F;        // 2 (PB0-3), PB4 je! des. tecka
+      PORTB = (PORTB & 0xF0) | 0x0F;        // 2 (PB0-3), PB4 je des. tecka
       isBlanking = true;
       timer_counter = OFFTIME;
     } 
@@ -257,15 +205,16 @@ ISR(TIMER1_COMPA_vect) {
       uint_fast8_t cathod_drv_1; 
       uint_fast8_t cathod_drv_2;
 
-      if (pairIndex == 0) {      // Anode A0: Tubes 1 and 5
-        cathod_drv_1 = displayBuffer.displayedMinutesTens;//tube 1 //DH
-        cathod_drv_2 = displayBuffer.displayedSecondsUnits; //tube 5 //DS
-      } else if (pairIndex == 1) { // Anode A1: Tubes 2 and 4
-        cathod_drv_1 = displayBuffer.displayedHoursUnits; //tube 2 //JH
-        cathod_drv_2 = displayBuffer.displayedSecondsTens; //tube 4 //JM
-      } else {                   // Anode A2: Tubes 3 and 6
-        cathod_drv_1 = displayBuffer.displayedHoursTens; //tube 3 // DM
-        cathod_drv_2 = displayBuffer.displayedMinutesUnits; //tube 6
+      // TODO dodelat desetecku
+      if (pairIndex == 0) {      // Anoda A0
+        cathod_drv_1 = displayBuffer.displayedMinutesTens;
+        cathod_drv_2 = displayBuffer.displayedSecondsUnits; 
+      } else if (pairIndex == 1) { // Anoda A1
+        cathod_drv_1 = displayBuffer.displayedHoursUnits; 
+        cathod_drv_2 = displayBuffer.displayedSecondsTens; 
+      } else {                   // Anoda A2
+        cathod_drv_1 = displayBuffer.displayedHoursTens; 
+        cathod_drv_2 = displayBuffer.displayedMinutesUnits; 
       }
       /* poznamka programatora - uint_fast8_t neni nic jineho nez fancy #define. radeji to ale napiseme,
       funguje to totiz jako jakysi mental reminder, ze jsme v ISR a tudiz program musi byt co nejmensi, nejrychlejsi*/
